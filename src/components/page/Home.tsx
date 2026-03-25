97fd9b870e032ba9d02e629d6a08379d03f52b3c
XDruidd <137764566+XDruidd@users.noreply.github.com>
2026-03-25 22:47:45 +0200

    Functions Block
    add Functions Block in home page

--- src/components/page/Home.tsx
+++ src/components/page/Home.tsx
@@ -1,10 +1,20 @@
 import { Box, Button, Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import AboutUs from '../../assets/oilAboutUs.jpg';
+import Detailed from '../../assets/functionImg/detailed.svg';
+import Food from '../../assets/functionImg/food.svg';
+import ListIco from '../../assets/functionImg/list.svg';
+import Nutritionist from '../../assets/functionImg/nutritionist.svg';
+import Oil from '../../assets/functionImg/oil.svg';
+import Scroll from '../../assets/functionImg/scroll.svg';
+
+const icons = [Oil, Scroll, ListIco, Food,Nutritionist, Detailed]
+
 function Home(){
     
     const {t} = useTranslation("home");
-
+    const features = t('features', { returnObjects: true });
+    
     return(
         <>
             <Box sx={{
@@ -130,10 +140,94 @@ function Home(){
                         lg: "25px 65px",
                         md: "25px 42.5px",
                         xs: "25px 20px"   
-                    }
+                    },
+                    display: "grid",
+                    gridTemplateColumns: {
+                        lg: "repeat(3, 1fr)", 
+                        md: "repeat(3, 1fr)", 
+                        xs: "repeat(2, 1fr)"
+                    },
+                    justifyContent: {
+                        lg: "space-between",
+                        xs: "space-around"
+                    },
+                    justifyItems: "center", 
+                    columnGap:"40px",
+                    rowGap: "20px"
+
                 }}
             >
-                
+                {Array.isArray(features) && features.map((item, index) => (
+                        <Box key={index} sx={{
+                            width:{
+                                lg: "363px",
+                                md: "265.5px",
+                                sm: "207.75px",
+                                xs: "150px"
+                            },
+                            display: "flex",
+                            flexDirection: "column",
+                            textAlign: {
+                                md: "left",
+                                sm: "center",
+                                xs: "center"
+                            },
+                            rowGap: {
+                                lg: "24px",
+                                xs: "12px" 
+                            },
+                        }}>
+                            <Box sx={{
+                              display: "flex",  
+                              justifyContent: "center",
+                            }}>
+                                <Box 
+                                    component={"img"}
+                                    src={icons[index]}
+                                    sx={{
+                                        width:{
+                                            lr: "196px",
+                                            md: "142.5px",
+                                            sm: "142.75px",
+                                            xs: "80.75px"
+                                        },
+                                        height:{
+                                            lr: "155px",
+                                            md: "112.5px",
+                                            sm: "155px",
+                                            xs: "65px"
+                                        },
+
+                                    }}
+                                />
+                            </Box>
+                            <Typography sx={{
+                                fontWeight: 700,
+                                fontSize: {
+                                    lg: "24px",
+                                    sm: "20px",
+                                    xs: "12px",
+                                },
+                                letterSpacing: 0,
+                            }}>
+                                {item.title}
+                            </Typography>
+                            <Typography sx={{
+                                fontWeight: 500,
+                                fontSize: {
+                                    lg: "24px",
+                                    sm: "20px",
+                                    xs: "12px",
+                                },
+                                letterSpacing: 0,
+                                lineHeight: "100%",
+                            }}>
+                                {item.description}
+                            </Typography>
+                        </Box>
+                    )
+                  )
+                }
             </Box>
         </>
     )
